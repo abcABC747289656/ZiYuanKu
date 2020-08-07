119053f679068f99c0f067e7b45bbc2eac84a881
wuyiquan <747289656@qq.com>
2020-08-07 10:38:48 +0800

    修改

--- mylibrary/src/main/java/com/example/mylibrary/ShiPin.java
+++ mylibrary/src/main/java/com/example/mylibrary/ShiPin.java
@@ -1,7 +1,7 @@
 package com.example.mylibrary;
 
 public class ShiPin {
-    String[] shipinbofang = {"https://top1-video-public.cdn.bcebos.com/97bfefaef0024885681ac693c58b0e901e881bf6.mp4",
+   public String[] shipinbofang = {"https://top1-video-public.cdn.bcebos.com/97bfefaef0024885681ac693c58b0e901e881bf6.mp4",
             "https://top1-video-public.cdn.bcebos.com/d2d0c13849f70fd626249c3ce8c4f465c7acb739.mp4",
             "https://top1-video-public.cdn.bcebos.com/cd6f31ff79622d4eb09609100a4b9960f26832b6.mp4"};
 }
