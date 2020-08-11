fd6d03580a9adad51dc815554dd178a141d80887
wuyiquan <747289656@qq.com>
2020-08-11 10:29:44 +0800

    修改

--- mylibrary/src/main/java/com/example/mylibrary/Save.java
+++ mylibrary/src/main/java/com/example/mylibrary/Save.java
@@ -6,7 +6,7 @@ public class Save {
             new ListItem("MySQL下载与安装教程视频","https://img2018.cnblogs.com/i-beta/1847065/201911/1847065-20191106171430629-25731686.png"),
             new ListItem("MySQL环境变量配置","https://img2018.cnblogs.com/blog/1216927/201907/1216927-20190721210423127-311832465.png"),
             new ListItem("SQL server下载与安装","https://upload-images.jianshu.io/upload_images/4055666-7764f967d8f9fa97.png?imageMogr2/auto-orient/strip|imageView2/2/w/509"),
-            new ListItem("Android Studio下载网与安装教程","https://img-blog.csdnimg.cn/20190903155812565.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MTM4NzU2,size_16,color_FFFFFF,t_70"),
+            new ListItem("Android Studio下载网址与安装教程","https://img-blog.csdnimg.cn/20190903155812565.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MTM4NzU2,size_16,color_FFFFFF,t_70"),
             new ListItem("PS下载网址与安装教程","https://www.seoxiehui.cn/data/attachment/portal/201908/23/145309tfl5e3nj3mw8l5s8.jpg"),
             new ListItem("IDEA下载与安装","https://img-blog.csdnimg.cn/20190525132345750.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80NDMyMzg2OQ==,size_16,color_FFFFFF,t_70"),
             new ListItem("AI下载网址与安装教程","https://i0.hdslb.com/bfs/article/619a01db7a2c1d982cc0c367b210c31a8963e8e2.jpg@1075w_602h.jpg"),
---
+++ mylibrary/src/main/java/com/example/mylibrary/ShiPinWeiZhi.java
@@ -0,0 +1,42 @@
+package com.example.mylibrary;
+
+public class ShiPinWeiZhi {
+    public int weizhi;
+    public String ChooseWeiZhi;
+    public void WeiZhiXuanZhe(){
+        switch (ChooseWeiZhi){
+            case "java环境变量win10":
+                weizhi = 0;
+                break;
+            case "eclipse安装教程":
+                weizhi = 1;
+                break;
+            case "MySQL下载与安装教程视频":
+                weizhi = 2;
+                break;
+            case "MySQL环境变量配置":
+                weizhi = 3;
+                break;
+            case "SQL server下载与安装":
+                weizhi = 4;
+                break;
+            case "Android Studio下载网址与安装教程":
+                weizhi = 5;
+                break;
+            case "PS下载网址与安装教程":
+                weizhi = 6;
+                break;
+            case "IDEA下载与安装":
+                weizhi = 7;
+                break;
+            case "AI下载网址与安装教程":
+                weizhi = 8;
+                break;
+            case "Matlab下载网址与安装教程":
+                weizhi = 9;
+                break;
+            default:
+                break;
+        }
+    }
+}
